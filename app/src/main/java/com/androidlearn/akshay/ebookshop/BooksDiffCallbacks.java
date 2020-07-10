8db4bacc75b0df93100c576967fd0e2ea9a56b8e
Akshay Choudhari <akshay.choudhari358@gmail.com>
2020-07-10 10:48:12 +0530

    Renamed Packages

---
+++ app/src/main/java/com/androidlearn/akshay/ebookshop/BooksDiffCallbacks.java
@@ -0,0 +1,45 @@
+package com.androidlearn.akshay.ebookshop;
+
+import android.support.annotation.Nullable;
+import android.support.v7.util.DiffUtil;
+
+import com.androidlearn.akshay.ebookshop.model.Book;
+
+import java.util.ArrayList;
+
+public class BooksDiffCallbacks extends DiffUtil.Callback {
+
+    ArrayList<Book> oldBooksList;
+    ArrayList<Book> newBooksList;
+
+    public BooksDiffCallbacks(ArrayList<Book> oldBooksList, ArrayList<Book> newBooksList) {
+        this.oldBooksList = oldBooksList;
+        this.newBooksList = newBooksList;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return oldBooksList==null?0:oldBooksList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return newBooksList==null?0:newBooksList.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldBookPosition, int newBookPosition) {
+        return oldBooksList.get(oldBookPosition).getBookId() == newBooksList.get(newBookPosition).getBookId();
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldBookPosition, int newBookPosition) {
+        return oldBooksList.get(oldBookPosition).equals(newBooksList.get(newBookPosition));
+    }
+
+    @Nullable
+    @Override
+    public Object getChangePayload(int oldBookPosition, int newBookPosition) {
+        return super.getChangePayload(oldBookPosition, newBookPosition);
+    }
+}
